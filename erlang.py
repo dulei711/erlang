dddc3c265ae4960a7d7b8c5d5649ec8337740d0d
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-11 22:36:43 -0300

    Update erlang.py

--- erlang.py
+++ erlang.py
@@ -6,6 +6,21 @@ def erlang_c(num_agents, traffic_intensity):
     b = sum(traffic_intensity ** i / factorial(i) for i in range(num_agents))
     return a / (a + b)
 
+def find_num_agents(service_level, traffic_intensity):
+    # Set the lower and upper bounds for the binary search
+    lower_bound = 1
+    upper_bound = 1000
+
+    # Perform binary search to find the number of agents
+    while lower_bound <= upper_bound:
+        mid = (lower_bound + upper_bound) // 2
+        if erlang_c(mid, traffic_intensity) < (service_level / 100):
+            lower_bound = mid + 1
+        else:
+            upper_bound = mid - 1
+
+    return lower_bound
+
 st.title('Call Center Staffing Calculator')
 
 call_volume = st.number_input('Enter predicted call volume per hour:', min_value=1, max_value=10000, value=100)
@@ -14,6 +29,6 @@ traffic_intensity = call_volume * (avg_handle_time / 60)
 
 service_level = st.slider('Select desired service level:', min_value=80, max_value=99, value=90, step=1)
 
-num_agents = sum(erlang_c(i, traffic_intensity) < (service_level / 100) for i in range(1, 1000))
+num_agents = find_num_agents(service_level, traffic_intensity)
 
 st.write(f'The required number of agents to achieve a service level of {service_level}% is {num_agents}.')
