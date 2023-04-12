bb33dce8babc38472518756e42a742fa2d9b4830
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-11 22:29:54 -0300

    Update erlang.py

--- erlang.py
+++ erlang.py
@@ -1,12 +1,11 @@
 import streamlit as st
+from decimal import Decimal
 import math
-import matplotlib.pyplot as plt
-import numpy as np
 
 def erlang_c(num_agents, traffic_intensity):
     a = (traffic_intensity ** num_agents) / math.factorial(num_agents)
     b = sum([(traffic_intensity ** i) / math.factorial(i) for i in range(num_agents)])
-    return a / (a + b)
+    return Decimal(a) / (Decimal(a) + Decimal(b))
 
 st.set_page_config(page_title="Call Center Staffing Calculator", page_icon=":telephone_receiver:")
 
@@ -18,23 +17,23 @@ call_volume = st.number_input('Enter predicted call volume per hour:', min_value
 
 avg_handle_time = st.number_input('Enter average call handling time (in minutes):', min_value=1, max_value=60, value=5)
 
-traffic_intensity = call_volume * (avg_handle_time / 60)
+traffic_intensity = Decimal(call_volume) * (Decimal(avg_handle_time) / Decimal(60))
 
 service_levels = list(range(80, 100, 1))
 
 num_agents_required = []
 for service_level in service_levels:
     num_agents = 1
-    while erlang_c(num_agents, traffic_intensity) < (service_level / 100):
+    while erlang_c(num_agents, traffic_intensity) < (Decimal(service_level) / Decimal(100)):
         num_agents += 1
     num_agents_required.append(num_agents)
 
-fig1, ax1 = plt.subplots()
-ax1.plot(service_levels, num_agents_required)
-ax1.set_title('Call Center Staffing Optimization')
-ax1.set_xlabel('Service Level')
-ax1.set_ylabel('Number of Agents Required')
-st.pyplot(fig1)
+# Show chart to visualize optimization
+st.subheader('Optimization for Required Number of Agents')
+chart_data = {"Service Levels": service_levels, "Agents Required": num_agents_required}
+df = pd.DataFrame(chart_data)
+fig = px.line(df, x="Service Levels", y="Agents Required")
+st.plotly_chart(fig, use_container_width=True)
 
 st.markdown('---')
 
@@ -48,7 +47,7 @@ erlang_avg_handle_time = st.number_input('Enter the average handling time per ca
 
 erlang_interval_time = st.number_input('Enter the length of the time interval (in minutes):', min_value=1, max_value=60, value=30)
 
-erlang_traffic_intensity = erlang_call_volume / (erlang_interval_time / erlang_avg_handle_time)
+erlang_traffic_intensity = Decimal(erlang_call_volume) / (Decimal(erlang_interval_time) / Decimal(erlang_avg_handle_time))
 
 erlang_num_agents = st.number_input('Enter the number of agents:', min_value=1, max_value=1000, value=1)
 
@@ -56,13 +55,13 @@ erlang_service_levels = list(range(80, 100, 1))
 
 erlang_service_levels_achieved = []
 for erlang_service_level in erlang_service_levels:
-    erlang_traffic_intensity_per_agent = erlang_traffic_intensity / erlang_num_agents
-    erlang_service_level_achieved = erlang_c(erlang_num_agents, erlang_traffic_intensity_per_agent) * 100
+    erlang_traffic_intensity_per_agent = erlang_traffic_intensity / Decimal(erlang_num_agents)
+    erlang_service_level_achieved = erlang_c(erlang_num_agents, erlang_traffic_intensity_per_agent) * Decimal(100)
     erlang_service_levels_achieved.append(erlang_service_level_achieved)
 
-fig2, ax2 = plt.subplots()
-ax2.plot(erlang_service_levels, erlang_service_levels_achieved)
-ax2.set_title('Erlang Calculator Optimization')
-ax2.set_xlabel('Desired Service Level')
-ax2.set_ylabel('Achieved Service Level')
-st.pyplot(fig2)
+# Show chart to visualize optimization
+st.subheader('Optimization for Achieved Service Levels')
+chart_data = {"Service Levels": erlang_service_levels, "Achieved Service Levels": erlang_service_levels_achieved}
+df = pd.DataFrame(chart_data)
+fig = px.line(df, x="Service Levels", y="Achieved Service Levels")
+st.plotly_chart(fig, use_container_width=True)
